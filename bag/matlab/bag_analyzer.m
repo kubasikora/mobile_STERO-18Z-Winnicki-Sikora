b424fa7e68a697a9dcabee0e941c895e34d82202
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2018-12-31 01:18:22 +0100

    .bag content plotter

---
+++ bag/matlab/bag_analyzer.m
@@ -0,0 +1,50 @@
+bag = rosbag('../test.bag');
+
+
+%% acquire odom data
+odom = select(bag, 'Topic', '/elektron/mobile_base_controller/odom');
+ts_odom_X = timeseries(odom, 'Pose.Pose.Position.X');
+ts_odom_Y = timeseries(odom, 'Pose.Pose.Position.Y');
+ts_odom_theta = timeseries(odom, 'Twist.Twist.Angular.Z');
+
+
+%% acquire gazebo data
+gazebo = select(bag, 'Topic', '/gazebo_odom');
+ts_gazebo_X = timeseries(gazebo, 'Pose.Pose.Position.X');
+ts_gazebo_Y = timeseries(gazebo, 'Pose.Pose.Position.Y');
+ts_gazebo_theta = timeseries(gazebo, 'Twist.Twist.Angular.Z');
+
+
+%% plot X data
+figure
+plot(ts_odom_X); 
+hold on; 
+grid on; 
+grid minor; 
+plot(ts_gazebo_X);
+title('Porównanie pozycji w współrzędnej X');
+legend('Robot', 'Reality');
+hold off;
+
+
+%% plot Y data
+figure
+plot(ts_odom_Y); 
+hold on; 
+grid on; 
+grid minor; 
+plot(ts_gazebo_Y);
+title('Porównanie pozycji w współrzędnej Y');
+legend('Robot', 'Reality');
+hold off;
+
+%% plot theta data
+figure
+plot(ts_odom_theta); 
+hold on; 
+grid on; 
+grid minor; 
+plot(ts_gazebo_theta);
+title('Porównanie pozycji w współrzędnej theta');
+legend('Robot', 'Reality');
+hold off;
